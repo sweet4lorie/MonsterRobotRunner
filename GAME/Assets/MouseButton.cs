a9e973714648ee047e02adda7f8c8923db9ce430
AndrewDieppa <andrewdieppa@gmail.com>
2014-11-05 11:08:50 -0800

    test add

---
+++ GAME/Assets/MouseButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseButton : MonoBehaviour
+{
+	void OnMouseDown()
+	{
+		Debug.Log(this.name + " Mouse Down");
+	}
+
+	void OnMouseUp()
+	{
+		Debug.Log(this.name + " Mouse Up");
+	}
+}

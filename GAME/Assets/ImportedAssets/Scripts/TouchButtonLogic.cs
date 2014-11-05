870af0a42f49b63da846262c8fcd7ca1d023c8fc
AndrewDieppa <andrewdieppa@gmail.com>
2014-11-05 15:45:12 -0800

    Changed the target platform to iOS. Added a simple touch input script to assets

---
+++ GAME/Assets/ImportedAssets/Scripts/TouchButtonLogic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchButtonLogic : MonoBehaviour
+{
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// check for touches on screen
+		if(Input.touches.Length <= 0)
+		{
+			//if no touches then do this
+		}
+		else //if there is a touch/es
+		{
+			//loop through all the touches
+			for (int i = 0; i < Input.touchCount; i++)
+			{
+				// execute code for current touch on screen (i)
+				if (this.guiTexture.HitTest(Input.GetTouch(i).position))
+				{
+					// if current touch is within our guiTexture
+					if(Input.GetTouch(i).phase == TouchPhase.Began)
+					{
+						Debug.Log ("The touch is down on " + this.name);
+					}
+					if(Input.GetTouch(i).phase == TouchPhase.Ended)
+					{
+						Debug.Log ("The touch has ended on " + this.name);
+					}
+				}
+			}
+		}
+
+	}
+}

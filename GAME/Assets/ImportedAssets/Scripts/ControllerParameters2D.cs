159ce254e5ed6448291c67d2cb97a9c738119a58
AndrewDieppa <andrewdieppa@gmail.com>
2014-11-14 14:44:09 -0800

    Added player controller scripts to assets

---
+++ GAME/Assets/ImportedAssets/Scripts/ControllerParameters2D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[Serializable]
+public class ControllerParameters2D
+{
+	public enum JumpBehavior
+	{
+		CanJumpOnGround,
+		CanJumpAnywhere,
+		CantJump
+	}
+	
+	public Vector2 MaxVelocity = new Vector2(float.MaxValue, float.MaxValue);
+	
+	[Range(0, 90)]
+	public float SlopeLimit = 30;
+	
+	public float Gravity = -25f;
+	
+	public JumpBehavior JumpRestrictions;
+	
+	public float JumpFrequency = .25f;
+	
+	public float JumpMagnitude = 12;
+}
+
---
+++ GAME/Assets/ImportedAssets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	private bool _isFacingRight;
+	private CharacterController2D _controller;
+	private float _normalizedHorizontalSpeed;
+	private GUIText _jumpButton;
+	
+	public float MaxSpeed = 8f;
+	public float SpeedAccelerationOnGround = 10f;
+	public float SpeedAccelerationInAir = 5f;
+	
+	public void Start()
+	{
+		
+		_controller = GetComponent<CharacterController2D>();
+		_jumpButton = GameObject.Find ("btnJump").guiText;
+		
+		_isFacingRight = transform.localScale.x > 0;
+	}
+	
+	
+	
+	public void Update()
+	{
+		HandleInput();
+		
+		var movementFactor = _controller.State.IsGrounded ? SpeedAccelerationOnGround : SpeedAccelerationInAir;
+		_controller.SetHorizontalForce(Mathf.Lerp(_controller.Velocity.x, _normalizedHorizontalSpeed * MaxSpeed, Time.deltaTime * movementFactor));
+	}
+	
+	public void OnGUI()
+	{
+		if(Input.touches.Length > 0)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began && _jumpButton.HitTest(Input.GetTouch(0).position))
+			{
+				if (_controller.CanJump)
+					_controller.Jump();
+			}
+		}
+	}
+	
+	private void HandleInput()
+	{
+		if(_controller.CanJump && Input.GetKeyDown(KeyCode.Space))
+		{
+			_controller.Jump();
+		}
+	}
+	
+	private void Flip()
+	{
+		transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+		_isFacingRight = transform.localScale.x > 0;
+	}
+}
+
